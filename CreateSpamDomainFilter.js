f1b44f5fc02f03e7c30ac2f71a33b440d05645be
Craig M. Rosenblum <605695+crosenblum@users.noreply.github.com>
2023-07-21 11:20:13 -0500

    Update CreateSpamDomainFilter.js
    Removed error logging. As it displayed no useful informatiom.

--- CreateSpamDomainFilter.js
+++ CreateSpamDomainFilter.js
@@ -9,7 +9,13 @@ function createDomainBlockFilter(emailAddress,domain) {
       In this case the criteria is that the email is sent from the same address as the sender of the current thread.
   */ 
   filter.criteria = Gmail.newFilterCriteria();
-  filter.criteria.from = domain;
+
+  /* if gmail use email address */
+  if (domain == 'gmail.com') {
+    filter.criteria.from = emailAddress;
+  } else {
+    filter.criteria.from = domain;
+  }
 
   /* 
       Filters in gmail have actions
@@ -24,20 +30,31 @@ function createDomainBlockFilter(emailAddress,domain) {
   filter.action.trash = true;
   filter.action.spam = true;
 
-  // Log that the filter was created
-  Logger.log("Creating Filter From: [" + emailAddress + "] Domain: [" + domain + "]");
+ /* try catch */
+ try {
+  /*
+      get the email address of the person whose account is running the script
+      In my case, this would be me. In your case, this will be your email.
+      We could also just use the string 'myemail@example.com', but I'm trying to avoid putting my personal email
+      in this code.
+    */
+    var me = Session.getEffectiveUser().getEmail();
 
- /*
-    get the email address of the person whose account is running the script
-    In my case, this would be me. In your case, this will be your email.
-    We could also just use the string 'myemail@example.com', but I'm trying to avoid putting my personal email
-    in this code.
-  */
-  var me = Session.getEffectiveUser().getEmail();
+    /* Create the filter */
+    var response = Gmail.Users.Settings.Filters.create(filter, me);
 
-  // Create the filter
-  var response = Gmail.Users.Settings.Filters.create(filter, me);
-
+    /* check if gmail.com domain */
+    if (domain == 'gmail.com') {
+      /* Log that the filter was created */
+      Logger.log("Creating Email Filter From: [" + emailAddress + "]");
+    } else {
+      /* Log that the filter was created */
+      Logger.log("Creating Domain Filter From: [" + domain + "]");
+    }
+ }
+ catch(err) {
+  /* Logger.log(err); */
+ }
 
 }
 
@@ -53,27 +70,34 @@ function getDomainFromEmailAddress(emailAddress) {
 }
 
 function GetSpamMessages() {
+  /* get all spam threads */
   var threads = GmailApp.getSpamThreads();
   
+  /* loop thru all threads */
   for (var i = 0; i < threads.length; i++) {
+
+    /* get all messages for this thread */
     var messages = threads[i].getMessages();
     
+    /* loop thru all messages */
     for (var j = 0; j < messages.length; j++) {
+      
+      /* get current message */
       var message = messages[j];
       
-      // get message id
+      /* get message id */
       var messageId = message.getId();
 
-      // now show from
+      /* now show from */
       var emailAddress = message.getFrom().replace(/^.+<([^>]+)>$/, "$1");
 
-      // get domain name
+      /* get domain name */
       var domain = getDomainFromEmailAddress(emailAddress);
 
-      // create new filter for this domain
+      /* create new filter for this domain */
       createDomainBlockFilter(emailAddress,domain);
 
-      // move message to trash
+      /* move message to trash */
       message.moveToTrash();
       
 
